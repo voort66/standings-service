6562cf25e47ad20c2216bf7cd1f58edfd9863e26
Wim van der Voort <wvoort.66@gmail.com>
2022-09-18 18:13:12 +0100

    first commit

---
+++ src/main/java/com/wvoort/wc2022/standingsservice/StandingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wvoort.wc2022.standingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandingsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvoort/wc2022/standingsservice/StandingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvoort.wc2022.standingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
